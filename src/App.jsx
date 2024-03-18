26418a9d836dc6c90ff856e5b7eaae353e94c798
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-03-18 10:42:21 +0530

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="destructive">Submit</Button>
+    </>
+  );
+}
+
+export default App;
